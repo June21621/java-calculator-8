c2e9bc290e8f7f0901b0a323df5e7cf106fe809a
june21621 <kymoon4@naver.com>
2025-10-20 16:03:57 +0900

    refactor: 문자열 계산 방식 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Application {
     // 커스텀 구분자를 ','로 바꾸는 함수
     private static String OtherSymbol(String s, char ch) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
         try {
             for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) == ch) {
-                    result += ',';
+                    result.append(',');
                 } else {
-                    result += s.charAt(i);
+                    result.append(s.charAt(i));
                 }
             }
-            return result;
+            return result.toString();
         }
         catch (Exception e) {
             throw new IllegalArgumentException("잘못된 값이 입력됨", e);
@@ -25,18 +25,18 @@ public class Application {
     // 문자열을 나눠서 더하는 함수
     private static int PlusMethod (String line) {
         int result = 0;
-        String token = "0";
+        StringBuilder token = new StringBuilder("0");
 
         try {
             for (int i = 0; i < line.length(); i++) {
                 if (line.charAt(i) == ',' || line.charAt(i) == ':') {
-                    result += Integer.parseInt(token);
-                    token = "0";
+                    result += Integer.parseInt(token.toString());
+                    token = new StringBuilder("0");
                 } else {
-                    token += line.charAt(i);
+                    token.append(line.charAt(i));
                 }
             }
-            result += Integer.parseInt(token);
+            result += Integer.parseInt(token.toString());
 
             return result;
         }
