02f076d408e882a8bd864e80c6a860d77f313c25
june21621 <kymoon4@naver.com>
2025-10-20 15:56:20 +0900

    feat: type 필수 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,69 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
+    // 커스텀 구분자를 ','로 바꾸는 함수
+    private static String OtherSymbol(String s, char ch) {
+        String result = "";
+
+        try {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == ch) {
+                    result += ',';
+                } else {
+                    result += s.charAt(i);
+                }
+            }
+            return result;
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("잘못된 값이 입력됨", e);
+        }
+    }
+
+    // 문자열을 나눠서 더하는 함수
+    private static int PlusMethod (String line) {
+        int result = 0;
+        String token = "0";
+
+        try {
+            for (int i = 0; i < line.length(); i++) {
+                if (line.charAt(i) == ',' || line.charAt(i) == ':') {
+                    result += Integer.parseInt(token);
+                    token = "0";
+                } else {
+                    token += line.charAt(i);
+                }
+            }
+            result += Integer.parseInt(token);
+
+            return result;
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("잘못된 값이 입력됨", e);
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        String inputWord;
+        char ch = ',';
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        try {
+            inputWord = scanner.nextLine();
+            // 커스텀 구분자를 사용할 경우 커스텀 구분자를 ,로 바꾸기
+            if (inputWord.startsWith("//")) {
+                ch = inputWord.charAt(2);
+                inputWord = OtherSymbol(scanner.next(), ch);
+            }
+            System.out.println("결과 : " + PlusMethod(inputWord));
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
