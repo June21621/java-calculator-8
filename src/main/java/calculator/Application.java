23245384a81a55ce633643289949114109533f2e
june21621 <kymoon4@naver.com>
2025-10-20 17:33:24 +0900

    refector : 조건에 맞게 변경 및 readme 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,25 +1,20 @@
 package calculator;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     // 커스텀 구분자를 ','로 바꾸는 함수
     private static String OtherSymbol(String s, char ch) {
         StringBuilder result = new StringBuilder();
 
-        try {
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == ch) {
-                    result.append(',');
-                } else {
-                    result.append(s.charAt(i));
-                }
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ch) {
+                result.append(',');
+            } else {
+                result.append(s.charAt(i));
             }
-            return result.toString();
         }
-        catch (Exception e) {
-            throw new IllegalArgumentException("잘못된 값이 입력됨", e);
-        }
+        return result.toString();
     }
 
     // 문자열을 나눠서 더하는 함수
@@ -27,43 +22,41 @@ public class Application {
         int result = 0;
         StringBuilder token = new StringBuilder("0");
 
-        try {
-            for (int i = 0; i < line.length(); i++) {
-                if (line.charAt(i) == ',' || line.charAt(i) == ':') {
-                    result += Integer.parseInt(token.toString());
-                    token = new StringBuilder("0");
-                } else {
-                    token.append(line.charAt(i));
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == ',' || line.charAt(i) == ':') {
+                int num = Integer.parseInt(token.toString());
+                if (num < 0) {
+                    throw new IllegalArgumentException("음수는 사용할 수 없습니다.");
                 }
+                result += num;
+                token = new StringBuilder("0");
+            } else {
+                token.append(line.charAt(i));
             }
-            result += Integer.parseInt(token.toString());
+        }
+        int num = Integer.parseInt(token.toString());
+        if (num < 0) {
+            throw new IllegalArgumentException("음수는 사용할 수 없습니다.");
+        }
+        result += num;
 
-            return result;
-        }
-        catch (Exception e) {
-            throw new IllegalArgumentException("잘못된 값이 입력됨", e);
-        }
+        return result;
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Scanner scanner = new Scanner(System.in);
         String inputWord;
         char ch = ',';
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        try {
-            inputWord = scanner.nextLine();
-            // 커스텀 구분자를 사용할 경우 커스텀 구분자를 ,로 바꾸기
-            if (inputWord.startsWith("//")) {
-                ch = inputWord.charAt(2);
-                inputWord = OtherSymbol(scanner.next(), ch);
-            }
-            System.out.println("결과 : " + PlusMethod(inputWord));
+        inputWord = Console.readLine();
+        // 커스텀 구분자를 사용할 경우 커스텀 구분자를 ,로 바꾸기
+        if (inputWord.startsWith("//")) {
+            ch = inputWord.charAt(2);
+            inputWord = inputWord.substring(5); // "//;\n" 이후 데이터
+            inputWord = OtherSymbol(inputWord, ch);
         }
-        catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        System.out.println("결과 : " + PlusMethod(inputWord));
 
     }
 }
